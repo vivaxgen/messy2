934d63a811a021ff359fe5d864f9f273b3af8dd9
anto @ alrescha <trimarsanto@gmail.com>
2026-05-03 17:04:06 +0930

    add roles

---
+++ src/messy2/lib/roles.py
@@ -0,0 +1,25 @@
+from litestar_pulse.lib.roles import *
+
+INSTITUTION_MANAGE = "~r|institution|manage"
+INSTITUTION_MODIFY = "~r|institution|modify"
+INSTITUTION_VIEW = "~r|institution|view"
+
+PROJECT_MANAGE = "~r|project|manage"
+PROJECT_MODIFY = "~r|project|modify"
+PROJECT_VIEW = "~r|project|view"
+
+SAMPLE_MANAGE = "~r|sample|manage"
+SAMPLE_MODIFY = "~r|sample|modify"
+SAMPLE_VIEW = "~r|sample|view"
+
+PLATE_MANAGE = "~r|plate|manage"
+PLATE_MODIFY = "~r|plate|modify"
+PLATE_VIEW = "~r|plate|view"
+
+SEQUENCINGRUN_MANAGE = "~r|sequencingrun|manage"
+SEQUENCINGRUN_MODIFY = "~r|sequencingrun|modify"
+SEQUENCINGRUN_VIEW = "~r|sequencingrun|view"
+
+SEQUENCE_MANAGE = "~r|sequence|manage"
+SEQUENCE_MODIFY = "~r|sequence|modify"
+SEQUENCE_VIEW = "~r|sequence|view"
