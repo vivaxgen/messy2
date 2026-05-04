dc4b554dbe101e0cd683512ee6962c087000263c
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-05 09:08:14 +0930

    convert and clean up old code

--- src/messy2/db/models/schema.py
+++ src/messy2/db/models/schema.py
@@ -1,4 +1,11 @@
+# SPDX-FileCopyrightText: 2026 Hidayat Trimarsanto <trimarsanto@gmail.com>
+# SPDX-License-Identifier: MPL-2.0
+
 from __future__ import annotations
+
+__copyright__ = "(C) 2026 Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__author__ = "trimarsanto@gmail.com"
+__license__ = "MPL-2.0"
 
 from datetime import date
 from typing import Any
@@ -7,7 +14,7 @@ from typing import Any
 from sqlalchemy import Column, Table, ForeignKey, UniqueConstraint, Identity
 from sqlalchemy.orm import DynamicMapped, Mapped, relationship, deferred, mapped_column
 from sqlalchemy.orm.collections import attribute_mapped_collection
-from sqlalchemy import types
+from sqlalchemy import types, func
 
 from advanced_alchemy.base import orm_registry
 from advanced_alchemy.types import JsonB
@@ -26,10 +33,10 @@ from litestar_pulse.db.models.account import Group, User
 
 from ...lib import roles as r
 
-MSY2_STORAGE = "messy2-storage"
+MESSy2_STORAGE = "messy2-storage"
 
-MSY2AttachedFiles = AttachedFiles(MSY2_STORAGE)
-MSY2Attachment = Attachment(MSY2_STORAGE)
+MESSy2AttachedFiles = AttachedFiles(MESSy2_STORAGE)
+MESSy2Attachment = Attachment(MESSy2_STORAGE)
 
 
 """
@@ -40,7 +47,7 @@ TODO:
 """
 
 
-class Institution(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class Institution(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
 
     __managing_roles__ = RoleMixin.__managing_roles__ | {r.INSTITUTION_MANAGE}
     __modifying_roles__ = __managing_roles__ | {r.INSTITUTION_MODIFY}
@@ -71,16 +78,14 @@ class Institution(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
 projects_institutions = Table(
     "projects_institutions",
     orm_registry.metadata,
-    Column("id", types.Integer, Identity(), primary_key=True),
-    Column("project_id", types.Integer, ForeignKey("projects.id"), nullable=False),
+    Column("project_id", types.Integer, ForeignKey("projects.id"), primary_key=True),
     Column(
-        "institution_id", types.Integer, ForeignKey("institutions.id"), nullable=False
+        "institution_id", types.Integer, ForeignKey("institutions.id"), primary_key=True
     ),
-    UniqueConstraint("project_id", "institution_id"),
 )
 
 
-class Project(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class Project(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
 
     __managing_roles__ = RoleMixin.__managing_roles__ | {r.PROJECT_MANAGE}
     __modifying_roles__ = __managing_roles__ | {r.PROJECT_MODIFY}
@@ -114,22 +119,25 @@ class Project(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     )
 
     samples: DynamicMapped[Sample] = relationship(
-        "Sample", lazy="dynamic", back_populates="collection", passive_deletes=True
+        "Sample", lazy="dynamic", back_populates="project", passive_deletes=True
     )
 
 
-class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
+class Sample(IdentityUUIDv7UserAuditBase, MESSy2Attachment, RoleMixin):
+    """
+    This class represent any Sample record
+    """
 
     __tablename__ = "samples"
 
-    collection_id: Mapped[int] = mapped_column(
+    project_id: Mapped[int] = mapped_column(
         types.Integer,
-        ForeignKey("collections.id", ondelete="CASCADE"),
+        ForeignKey("projects.id", ondelete="CASCADE"),
         nullable=False,
         index=True,
     )
-    collection: Mapped[Any] = relationship(
-        "Collection", uselist=False, back_populates="samples"
+    project: Mapped[Any] = relationship(
+        "Project", uselist=False, back_populates="samples"
     )
 
     # various code
@@ -144,12 +152,12 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
     species_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     species = enumkey_proxy("species_id", "@SPECIES")
 
     passage_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     passage = enumkey_proxy("passage_id", "@PASSAGE")
 
@@ -164,7 +172,7 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
     host_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     host = enumkey_proxy("host_id", "@SPECIES")
 
@@ -179,12 +187,12 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
     host_occupation_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     host_occupation = enumkey_proxy("host_occupation_id", "@HOST_OCCUPATION")
 
     host_status_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     host_status = enumkey_proxy("host_status_id", "@HOST_STATUS")
 
@@ -208,12 +216,12 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
     category_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     category = enumkey_proxy("category_id", "@CATEGORY")
 
     specimen_type_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     specimen_type = enumkey_proxy("specimen_type_id", "@SPECIMEN_TYPE")
 
@@ -254,7 +262,7 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
     ct_method_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     ct_method = enumkey_proxy("ct_method_id", "@CT_METHOD")
 
@@ -311,16 +319,8 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
         mapped_column(types.JSON, nullable=False, server_default="null")
     )
 
-    platepositions: Mapped[list[PlatePosition]] = relationship(
-        "PlatePosition", back_populates="sample", passive_deletes=True
-    )
-
-    sequences: DynamicMapped[Any] = relationship(
-        "Sequence", lazy="dynamic", back_populates="sample", passive_deletes=True
-    )
-
-    lineages: Mapped[list[Any]] = relationship(
-        "Lineage", back_populates="sample", passive_deletes=True
+    positions: Mapped[list[LabwarePosition]] = relationship(
+        "LabwarePosition", back_populates="sample", passive_deletes=True
     )
 
     __table_args__ = (
@@ -343,7 +343,7 @@ class Sample(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     __modifying_roles__ = __managing_roles__ | {r.SAMPLE_MODIFY}
 
 
-class Subject(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
+class Subject(IdentityUUIDv7UserAuditBase, MESSy2Attachment, RoleMixin):
     """
     This class represent an individual subject (person/patient/host) record
     """
@@ -371,43 +371,7 @@ class Subject(IdentityUUIDv7UserAuditBase, MSY2Attachment, RoleMixin):
     )
 
 
-class PlatePosition(IdentityUserAuditBase, RoleMixin):
-
-    __tablename__ = "platepositions"
-
-    labware_id: Mapped[int] = mapped_column(
-        types.Integer,
-        ForeignKey("labwares.id", ondelete="CASCADE"),
-        index=True,
-        nullable=False,
-    )
-    labware: Mapped[Labware] = relationship(
-        "Labware", uselist=False, foreign_keys=labware_id, back_populates="positions"
-    )
-
-    sample_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("samples.id"), index=True, nullable=False
-    )
-    sample: Mapped[Sample] = relationship(
-        Sample, uselist=False, foreign_keys=sample_id, back_populates="platepositions"
-    )
-
-    # position will be 384:A01 -> P24, 96: A01 -> H12
-    position: Mapped[str] = mapped_column(
-        types.String(3), nullable=False, server_default=""
-    )
-    value: Mapped[float] = mapped_column(
-        types.Float, nullable=False, server_default="-1"
-    )
-    volume: Mapped[float] = mapped_column(
-        types.Float, nullable=False, server_default="-1"
-    )
-    note: Mapped[str | None] = mapped_column(types.String(31), nullable=True)
-
-    __table_args__ = (UniqueConstraint("labware_id", "position"),)
-
-
-class Labware(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class Labware(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
     """
     This class represent a labware (plate, tube, etc.) record
     """
@@ -433,12 +397,12 @@ class Labware(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     )
 
     specimen_type_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     specimen_type = enumkey_proxy("specimen_type_id", "@SPECIMEN_TYPE")
 
     experiment_type_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
     experiment_type = enumkey_proxy("experiment_type_id", "@EXPERIMENT_TYPE")
 
@@ -449,16 +413,25 @@ class Labware(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
         mapped_column(types.Text, nullable=False, server_default="")
     )
 
-    positions: Mapped[list[PlatePosition]] = relationship(
-        PlatePosition,
-        order_by="PlatePosition.id",
+    positions: Mapped[list[LabwarePosition]] = relationship(
+        "LabwarePosition",
+        order_by="LabwarePosition.id",
         passive_deletes=True,
         back_populates="labware",
     )
 
+    storageunit_id: Mapped[int | None] = mapped_column(
+        types.Integer, ForeignKey("storageunits.id", ondelete="SET NULL"), nullable=True
+    )
+    storageunit: Mapped[StorageUnit | None] = relationship(
+        "StorageUnit",
+        back_populates="labwares",
+        passive_deletes=True,
+    )
+
     sequencingruns: Mapped[list[SequencingRunPlate]] = relationship(
         "SequencingRunPlate",
-        order_by="sequencingrunplates.c.plate_id",
+        order_by="sequencingrunplates.c.labware_id",
         back_populates="plate",
     )
 
@@ -468,28 +441,7 @@ class Labware(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     __modifying_roles__ = __managing_roles__ | {r.PLATE_MODIFY}
 
 
-plate_file_table = Table(
-    "plates_files",
-    metadata,
-    Column("id", types.Integer, Identity(), primary_key=True),
-    Column(
-        "plate_id",
-        types.Integer,
-        ForeignKey("plates.id", ondelete="CASCADE"),
-        index=True,
-        nullable=False,
-    ),
-    Column(
-        "file_id",
-        types.Integer,
-        ForeignKey("fileattachments.id", ondelete="CASCADE"),
-        nullable=False,
-    ),
-    UniqueConstraint("plate_id", "file_id"),
-)
-
-
-class StorageUnit(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class StorageUnit(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
     """
     This class represent a storage unit (freezer, shelf, box, etc.) record
     """
@@ -512,6 +464,12 @@ class StorageUnit(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
         mapped_column(types.JSON, nullable=False, server_default="null")
     )
 
+    labwares: Mapped[list[Labware]] = relationship(
+        Labware,
+        back_populates="storageunit",
+        passive_deletes=True,
+    )
+
     group_id: Mapped[int] = mapped_column(
         types.Integer, ForeignKey("groups.id"), nullable=False
     )
@@ -522,32 +480,34 @@ class LabwarePosition(IdentityUserAuditBase, RoleMixin):
 
     __tablename__ = "labwarepositions"
 
-    storageunit_id: Mapped[int] = mapped_column(
-        types.Integer,
-        ForeignKey("storageunits.id", ondelete="CASCADE"),
-        index=True,
-        nullable=False,
-    )
-    storageunit: Mapped[StorageUnit] = relationship(
-        StorageUnit,
-        uselist=False,
-        foreign_keys=storageunit_id,
-        back_populates="labwares",
-    )
-
     labware_id: Mapped[int] = mapped_column(
         types.Integer, ForeignKey("labwares.id"), index=True, nullable=False
     )
     labware: Mapped[Labware] = relationship(
-        Labware, uselist=False, foreign_keys=labware_id, back_populates="storageunits"
+        Labware,
+        uselist=False,
+        foreign_keys=labware_id,
+        back_populates="positions",
     )
+
+    sample_id: Mapped[int] = mapped_column(
+        types.Integer, ForeignKey("samples.id"), index=True, nullable=True
+    )
+    sample: Mapped[Sample] = relationship(Sample, uselist=False, foreign_keys=sample_id)
 
     position: Mapped[str] = mapped_column(
-        types.String(16), nullable=False, server_default=""
+        types.String(3), nullable=False, server_default=""
     )
+    value: Mapped[float] = mapped_column(
+        types.Float, nullable=False, server_default="-1"
+    )
+    volumne: Mapped[float] = mapped_column(
+        types.Float, nullable=False, server_default="-1"
+    )
+    note: Mapped[str | None] = mapped_column(types.String(31), nullable=True)
 
 
-class SequencingRun(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class SequencingRun(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
     """
     This class represent a libprep + sequencing run
     """
@@ -579,21 +539,17 @@ class SequencingRun(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     )
 
     sequencing_kit_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
-    sequencing_kit = EK.proxy("sequencing_kit_id", "@SEQUENCING_KIT")
+    sequencing_kit = enumkey_proxy("sequencing_kit_id", "@SEQUENCING_KIT")
 
     remark: Mapped[str] = deferred(
         mapped_column(types.Text, nullable=False, server_default="")
     )
 
-    sequences: DynamicMapped[Any] = relationship(
-        "Sequence", lazy="dynamic", back_populates="sequencingrun", passive_deletes=True
-    )
-
     plates: Mapped[list[SequencingRunPlate]] = relationship(
         "SequencingRunPlate",
-        order_by="sequencingrunplates.c.plate_id",
+        order_by="sequencingrunplates.c.labware_id",
         back_populates="sequencingrun",
     )
 
@@ -653,7 +609,7 @@ class SequencingRun(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     def as_dict(self, exclude=None):
         d = super().as_dict(exclude={"sequences", "plates", "additional_files"})
         d["plates"] = [
-            [p.plate.code, p.adapterindex, p.lane, p.note] for p in self.plates
+            [p.labware.code, p.adapterindex, p.lane, p.note] for p in self.plates
         ]
         return d
 
@@ -661,10 +617,10 @@ class SequencingRun(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
     def from_dict(cls, a_dict, dbh):
         run = super().from_dict(a_dict, dbh)
         for rp in a_dict.get("plates", []):
-            plate = dbh.get_plates_by_codes(rp[0], groups=None, ignore_acl=True)[0]
+            labware = dbh.get_labwares_by_codes(rp[0], groups=None, ignore_acl=True)[0]
             d = dict(
                 sequencingrun_id=run.id,
-                plate_id=plate.id,
+                labware_id=labware.id,
                 adapterindex=rp[1],
                 lane=rp[2],
                 note=rp[3],
@@ -679,7 +635,7 @@ class SequencingRun(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
         return False
 
 
-class SequencingRunPlate(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMixin):
+class SequencingRunPlate(IdentityUUIDv7UserAuditBase, MESSy2AttachedFiles, RoleMixin):
 
     __tablename__ = "sequencingrunplates"
 
@@ -696,20 +652,20 @@ class SequencingRunPlate(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMix
         back_populates="plates",
     )
 
-    plate_id: Mapped[int] = mapped_column(
+    labware_id: Mapped[int] = mapped_column(
         types.Integer,
-        ForeignKey("plates.id", ondelete="CASCADE"),
+        ForeignKey("labwares.id", ondelete="CASCADE"),
         index=True,
         nullable=False,
     )
     plate: Mapped[Labware] = relationship(
-        Labware, uselist=False, foreign_keys=plate_id, back_populates="sequencingruns"
+        Labware, uselist=False, foreign_keys=labware_id, back_populates="sequencingruns"
     )
 
     adapterindex_id: Mapped[int] = mapped_column(
-        types.Integer, ForeignKey("eks.id"), nullable=False
+        types.Integer, ForeignKey("enumkeys.id"), nullable=False
     )
-    adapterindex = EK.proxy("adapterindex_id", "@ADAPTERINDEX")
+    adapterindex = enumkey_proxy("adapterindex_id", "@ADAPTERINDEX")
 
     lane: Mapped[int] = mapped_column(types.Integer, nullable=False, server_default="1")
 
@@ -718,7 +674,7 @@ class SequencingRunPlate(IdentityUUIDv7UserAuditBase, MSY2AttachedFiles, RoleMix
     __ek_fields__ = {"adapterindex"}
 
     __table_args__ = (
-        UniqueConstraint("sequencingrun_id", "plate_id"),
+        UniqueConstraint("sequencingrun_id", "labware_id"),
         UniqueConstraint("sequencingrun_id", "adapterindex_id", "lane"),
     )
 
