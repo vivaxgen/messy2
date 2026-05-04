f24328f6f083e2a6f46482abbc9b7a3911efaae2
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-05 09:08:56 +0930

    use custom LPBaseService

--- src/messy2/db/handler.py
+++ src/messy2/db/handler.py
@@ -14,7 +14,7 @@ from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService
 
 import lazy_object_proxy as lop
 
-from litestar_pulse.db.handler import LPHandler
+from litestar_pulse.db import handler, set_handler_class
 
 from .models import schema
 
@@ -25,7 +25,7 @@ class InstitutionRepo(SQLAlchemyAsyncRepository[schema.Institution]):
     model_type = schema.Institution
 
 
-class InstitutionService(SQLAlchemyAsyncRepositoryService[schema.Institution]):
+class InstitutionService(handler.LPBaseService[schema.Institution]):
     repository_type = InstitutionRepo
 
     async def before_update_from_dict(
@@ -43,7 +43,7 @@ class ProjectRepo(SQLAlchemyAsyncRepository[schema.Project]):
     model_type = schema.Project
 
 
-class ProjectService(SQLAlchemyAsyncRepositoryService[schema.Project]):
+class ProjectService(handler.LPBaseService[schema.Project]):
     repository_type = ProjectRepo
 
     async def before_update_from_dict(
@@ -54,20 +54,20 @@ class ProjectService(SQLAlchemyAsyncRepositoryService[schema.Project]):
             await self.update_fileobject_list(instance, "files", data)
 
 
-class MSY2Model:
+class Model(handler.Model):
     Institution = schema.Institution
     Project = schema.Project
     Sample = schema.Sample
     Subject = schema.Subject
 
 
-class MSY2Handler(LPHandler):
+class MESSy2Handler(handler.LPHandler):
 
-    model = MSY2Model()
+    model = Model()
 
     def __init__(self, session: AsyncSession) -> None:
 
-        super().__init__(session)
+        super().__init__(session=session)  # type: ignore
 
         # preapre all AsyncRepos & AsyncServices
 
@@ -84,3 +84,8 @@ class MSY2Handler(LPHandler):
                 session=self.session, repository=self.repo.Project.__wrapped__
             )
         )
+
+
+set_handler_class(MESSy2Handler)
+
+# EOF
