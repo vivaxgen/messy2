366614bd57bef64b01e7ce8cea47b6154f24ed8b
anto @ neuromancer <trimarsanto@gmail.com>
2026-05-13 21:20:39 +0930

    update: migrate to pixi-based vvg-box

--- etc/inst-scripts/inst-stage-2.sh
+++ etc/inst-scripts/inst-stage-2.sh
@@ -1,4 +1,4 @@
-#!.usr/bin/env bash
+#!/usr/bin/env bash
 
 echo "Performing 2nd stage installation for MESSy2"
 
--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # installation script for litestar-pulse [https://github.com/trmznt/litestar-pulse]
 
 # optional variable:
-# - BASEDIR
-# - OMIT
+# - VVG_BASEDIR
+# - VVG_EXCLUDE
+# - MESSY2_REPOURL
 
 set -eu
 
@@ -24,21 +25,21 @@ case "$parent" in
 esac
 
 # Parsing arguments
-if [ -t 0 ] && [ -z "${BASEDIR:-}" ]; then
+if [ -t 0 ] && [ -z "${VVG_BASEDIR:-}" ]; then
   printf "Base installation directory? [./messy2] "
-  read BASEDIR
+  read VVG_BASEDIR
 fi
 
 # default value
-BASEDIR="${BASEDIR:-./messy2}"
+VVG_BASEDIR="${VVG_BASEDIR:-./messy2}"
 
-uMAMBA_ENVNAME="${uMAMBA_ENVNAME:-messy2}"
+PIXI_ENVNAME="${PIXI_ENVNAME:-messy2}"
 
 # install litestar-pulse
 source <(curl -L https://raw.githubusercontent.com/trmznt/litestar-pulse/main/install.sh)
 
 echo "Cloning MESSy2"
-git clone --depth 1 https://github.com/vivaxgen/messy2.git ${ENVS_DIR}/messy2
+git clone --depth 1 ${MESSY2_REPOURL:-https://github.com/vivaxgen/messy2.git} ${ENVS_DIR}/messy2
 
 # perform 2nd stage installation for MESSy2
 source ${ENVS_DIR}/messy2/etc/inst-scripts/inst-stage-2.sh
@@ -51,12 +52,12 @@ echo "MESSy2 has been successfully installed. "
 echo "Please read the docs for further setup."
 echo "The base installation directory (VVG_BASEDIR) is:"
 echo
-echo `realpath ${BASEDIR}`
+echo "$(realpath "${VVG_BASEDIR}")"
 echo
 echo "To activate the basic MESSy2 environment (eg. for installing"
 echo "or setting up base enviroment directory), execute the command:"
 echo
-echo `realpath ${BASEDIR}`/bin/activate
+echo "    $(realpath "${BINDIR}/activate")"
 echo
 
 # EOF
